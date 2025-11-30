81b9828aa35a4f301312c9c5a9400011aa119580
jorge <jorge@RAX445>
2025-11-29 20:51:16 -0600

    Agregacion de QuickSort

---
+++ Parcial 3/QuickSort/QuickSort/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace QuickSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numeros = { 3, 2, 1, 5, 4 };
+
+            QuickSort(numeros, 0, numeros.Length - 1);
+
+            Console.WriteLine("Array ordenado:");
+            foreach (var numero in numeros)
+            {
+                Console.WriteLine(numero);
+            }
+
+            void QuickSort(int[] arr, int inicio, int fin)
+            {
+                if (inicio < fin)
+                {
+                    int indicePivote = Particionar(arr, inicio, fin);
+                    QuickSort(arr, inicio, indicePivote - 1);
+                    QuickSort(arr, indicePivote + 1, fin);
+                }
+            }
+
+            int Particionar(int[] arr, int inicio, int fin)
+            {
+                int pivote = arr[fin];
+                int i = inicio - 1;
+
+                for (int j = inicio; j < fin; j++)
+                {
+                    if (arr[j] < pivote)
+                    {
+                        i++;
+                        Intercambiar(arr, i, j);
+                    }
+                }
+
+                Intercambiar(arr, i + 1, fin);
+                return i + 1;
+            }
+
+            void Intercambiar(int[] arr, int a, int b)
+            {
+                int temp = arr[a];
+                arr[a] = arr[b];
+                arr[b] = temp;
+            }
+        }
+    }
+}
